124b6dae52d2fce12c7ec1f88427d63185f3a6a0
CHHINKEO.CHHOEUNH <CHHINKEO.CHHOEUNH@PNC.LAN>
2023-08-02 15:03:45 +0700

    mer lg lg tv

---
+++ answer.py
@@ -0,0 +1,34 @@
+# S7
+# EX1
+# text=str(input())
+# sum=0
+# is_seven_number=False
+# for i in range(len(text)):	
+#     if int(text[i]) !=7 and is_seven_number==False :
+#         sum=sum+int(text[i])
+#     else:
+# 	    is_seven_number=True
+# print(sum)
+
+
+#EX2
+# text=input()
+# result=0
+# is_seven_number=True
+# for i in range(len(text)):
+#     if text[i]=="7" and is_seven_number==True:
+#         is_seven_number=False
+#     elif is_seven_number==False:
+#         result=result+int(text[i])
+# print(result)
+
+
+# EX3
+# text=input()
+# result=0
+# i=0
+# while i<len(text):
+#     if text[i]=="a" and text[i+1]=="b":
+#         result=result+1
+#     i=i+1
+# print(result)
